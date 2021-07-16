af980cbc5493cec4556ba3cb1ea09f9db6817432
Avi-Cohen <aviko.cohen@gmail.com>
2021-07-16 14:47:00 +0300

    wrote a 1st real test

--- __tests__/language_spoken.test.js
+++ __tests__/language_spoken.test.js
@@ -1,9 +1,27 @@
 const {
-    capitalize,
-    getAlpha2Code,
-    countryExtractor,
-    countryListLookup,
-    getResponse
-  } = require("../language_spoken")
-  test("convert array of country data objects to array of countries", ()=> {})
-  test("convert array of country data objects to array of countries 2", ()=> {})
+  capitalize,
+  getAlpha2Code,
+  countryExtractor,
+  countryListLookup,
+  getResponse,
+} = require("../language_spoken");
+
+test("convert array of country data objects to array of countries", () => {
+  //arrange
+  const inputObject = [
+    { name: "Argentina", capital: "Buenos Aires" },
+    { name: "Belize", capital: "Belmopan" },
+    { name: "Bolivia", capital: "Sucre" },
+  ];
+  const expectedValue = ["Argentina", "Belize", "Bolivia"];
+
+  //act
+  const actualValue = countryExtractor(inputObject);
+
+  //assertions
+  expect(actualValue).toEqual(expectedValue);
+});
+
+/*
+Note: The .toBe() matcher can be used to compare simple data types for equality while the .toEqual() matcher is used to perform deep equality comparisons. 
+*/
